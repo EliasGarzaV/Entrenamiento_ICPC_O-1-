0882b53359a03df9820bc307acaac20b4d062560
Alam Sanchez <asancheztorres1905@gmail.com>
2023-03-09 19:55:33 -0600

    Init notebook

---
+++ Notebook/Indexed Set.cpp
@@ -0,0 +1,18 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+// #include <bits/extc++.h>  // pbds
+using namespace __gnu_pbds;
+
+/**
+ * @brief Set data structure with rank - know which element is the kth 
+ * smallest.
+ * 
+ * find_by_order(n) => (n - 1)th smallest key
+ * order_of_key(k) => rank of key k
+ * 
+ * @tparam TKey Type of set key
+ */
+template <class TKey>
+using indexed_set = tree<TKey, null_type, less<Tkey>, rb_tree_tag,
+                         tree_order_statistics_node_update>;
---
+++ Notebook/Min Cost Matching.cpp
@@ -0,0 +1,104 @@
+
+#include <vector>
+using namespace std;
+template<class T>
+using mat = vector<vector<T>>;
+typedef vector<int> vi;
+
+/**
+ * Minimum Cost Matching in bipartite graph. Negate cost for Max.
+ * 
+ * @tparam TCost Type of the edge cost 
+ * @param cost Cost matrix. cost[i][j] = cost of pairing left i with right j
+ * @param Lmate Lmate[i] = index of right node that left node i pairs with
+ * @param Rmate Lmate[j] = index of right node that left node k pairs with
+ * @return TCost Minimum cost in perfect match.
+ */
+template<class TCost>
+TCost MinCostMatching(const mat<TCost> &cost, vi &Lmate, vi &Rmate) {
+    typedef vector<TCost> vc;
+    int n = int(cost.size());
+    // construct dual feasible solution
+    vc u(n), v(n);
+    for (int i = 0; i < n; i++) {
+        u[i] = cost[i][0];
+        for (int j = 1; j < n; j++) u[i] = min(u[i], cost[i][j]);
+    }
+    for (int j = 0; j < n; j++) {
+        v[j] = cost[0][j] - u[0];
+        for (int i = 1; i < n; i++) v[j] = min(v[j], cost[i][j] - u[i]);
+    }
+
+    // construct primal solution satisfying complementary slackness
+    Lmate = vi(n, -1);
+    Rmate = vi(n, -1);
+    int mated = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (Rmate[j] != -1) continue;
+            if (fabs(cost[i][j] - u[i] - v[j]) < 1e-10) {
+                Lmate[i] = j;
+                Rmate[j] = i;
+                mated++;
+                break;
+            }
+        }
+    }
+
+    vc dist(n);
+    vi dad(n), seen(n);
+    // repeat until primal solution is feasible
+    while (mated < n) {
+        // find an unmatched left node
+        int s = 0;
+        while (Lmate[s] != -1) s++;
+        // initialize Dijkstra
+        fill(dad.begin(), dad.end(), -1);
+        fill(seen.begin(), seen.end(), 0);
+        for (int k = 0; k < n; k++) dist[k] = cost[s][k] - u[s] - v[k];
+        int j = 0;
+        while (true) {
+            // find closest
+            j = -1;
+            for (int k = 0; k < n; k++) {
+                if (seen[k]) continue;
+                if (j == -1 || dist[k] < dist[j]) j = k;
+            }
+            seen[j] = 1;
+            // termination condition
+            if (Rmate[j] == -1) break;
+            // relax neighbors
+            const int i = Rmate[j];
+            for (int k = 0; k < n; k++) {
+                if (seen[k]) continue;
+                const ull new_dist = dist[j] + cost[i][k] - u[i] - v[k];
+                if (dist[k] > new_dist) {
+                    dist[k] = new_dist;
+                    dad[k] = j;
+                }
+            }
+        }
+        // update dual variables
+        for (int k = 0; k < n; k++) {
+            if (k == j || !seen[k]) continue;
+            const int i = Rmate[k];
+            v[k] += dist[k] - dist[j];
+            u[i] -= dist[k] - dist[j];
+        }
+        u[s] += dist[j];
+        // augment along path
+        while (dad[j] >= 0) {
+            const int d = dad[j];
+            Rmate[j] = Rmate[d];
+            Lmate[Rmate[j]] = j;
+            j = d;
+        }
+        Rmate[j] = s;
+        Lmate[s] = j;
+        mated++;
+    }
+    
+    TCost value = 0;
+    for (int i = 0; i < n; i++) value += cost[i][Lmate[i]];
+    return value;
+}
