37ccde74974f85f8128547aeefd223f5f0779b05
Alam Sanchez <asancheztorres1905@gmail.com>
2023-03-16 19:19:13 -0600

    Graphs

--- Notebook/template.cpp
+++ Notebook/template.cpp
@@ -6,7 +6,10 @@
 #define snd snd
 #define sz(x) x.size()
 #define rep(i, a, b) for (int i = a; i < (b); i++)
-#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define sync                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 #define all(x) x.begin(), x.end()
 
 using namespace std;
@@ -14,4 +17,5 @@ using namespace std;
 typedef unsigned long long ull;
 typedef long long ll;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 typedef pair<int, int> pii;
